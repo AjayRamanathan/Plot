236b0aac81f8fad7e3a3a797e4182ac42d0a7c87
Ajay Ramanathan <ajay.010293@gmail.com>
2015-04-19 00:12:57 +0530

    First commit

--- Plot.hs
+++ Plot.hs
@@ -5,100 +5,240 @@ import Diagrams.Backend.Cairo.CmdLine
 import Diagrams.TwoD.Text
 import Diagrams.Core.Points
 
-type Dia = Diagram B R2
+type DiaR2 = Diagram B R2
 
-type DataInt = ([String], [Double], [Int])
-type DataString = ([String], [String], [String])
+data DataFrame = DataDouble String [Double] (Double,Double) | DataString String [String] [String]
 
-city :: DataString
-city = (["city"],["Tokyo","Mumbai","New York","Los Angeles","Paris","Moscow","London","Chicago","Toronto","Berlin","Melbourne","Paris","Moscow","Toronto","Berlin","Melbourne"],["Tokyo","Mumbai","New York","Los Angeles","Paris","Moscow","London","Chicago","Toronto","Berlin","Melbourne"])
+iris = [(5.1,3.5,1.4,0.2,"setosa","A"),
+        (4.9,3.0,1.4,0.2,"setosa","A"),
+        (4.7,3.2,1.3,0.2,"setosa","A"),
+        (4.6,3.1,1.5,0.2,"setosa","A"),
+        (5.0,3.6,1.4,0.2,"setosa","A"),
+        (5.4,3.9,1.7,0.4,"setosa","B"),
+        (4.6,3.4,1.4,0.3,"setosa","B"),
+        (5.0,3.4,1.5,0.2,"setosa","B"),
+        (4.4,2.9,1.4,0.2,"setosa","B"),
+        (4.9,3.1,1.5,0.1,"setosa","B"),
+        (5.4,3.7,1.5,0.2,"setosa","B"),
+        (4.8,3.4,1.6,0.2,"setosa","B"),
+        (4.8,3.0,1.4,0.1,"setosa","B"),
+        (4.3,3.0,1.1,0.1,"setosa","B"),
+        (5.8,4.0,1.2,0.2,"setosa","B"),
+        (5.7,4.4,1.5,0.4,"setosa","B"),
+        (5.4,3.9,1.3,0.4,"setosa","D"),
+        (5.1,3.5,1.4,0.3,"setosa","D"),
+        (5.7,3.8,1.7,0.3,"setosa","D"),
+        (5.1,3.8,1.5,0.3,"setosa","D"),
+        (5.4,3.4,1.7,0.2,"setosa","D"),
+        (5.1,3.7,1.5,0.4,"setosa","D"),
+        (4.6,3.6,1.0,0.2,"setosa","B"),
+        (5.1,3.3,1.7,0.5,"setosa","B"),
+        (4.8,3.4,1.9,0.2,"setosa","D"),
+        (5.0,3.0,1.6,0.2,"setosa","F"),
+        (5.0,3.4,1.6,0.4,"setosa","C"),
+        (5.2,3.5,1.5,0.2,"setosa","C"),
+        (5.2,3.4,1.4,0.2,"setosa","C"),
+        (4.7,3.2,1.6,0.2,"setosa","C"),
+        (4.8,3.1,1.6,0.2,"setosa","C"),
+        (5.4,3.4,1.5,0.4,"setosa","F"),
+        (5.2,4.1,1.5,0.1,"setosa","F"),
+        (5.5,4.2,1.4,0.2,"setosa","D"),
+        (4.9,3.1,1.5,0.1,"setosa","D"),
+        (5.0,3.2,1.2,0.2,"setosa","D"),
+        (5.5,3.5,1.3,0.2,"setosa","E"),
+        (4.9,3.1,1.5,0.1,"setosa","E"),
+        (4.4,3.0,1.3,0.2,"setosa","E"),
+        (5.1,3.4,1.5,0.2,"setosa","C"),
+        (5.0,3.5,1.3,0.3,"setosa","C"),
+        (4.5,2.3,1.3,0.3,"setosa","C"),
+        (4.4,3.2,1.3,0.2,"setosa","C"),
+        (5.0,3.5,1.6,0.6,"setosa","E"),
+        (5.1,3.8,1.9,0.4,"setosa","F"),
+        (4.8,3.0,1.4,0.3,"setosa","F"),
+        (5.1,3.8,1.6,0.2,"setosa","F"),
+        (4.6,3.2,1.4,0.2,"setosa","C"),
+        (5.3,3.7,1.5,0.2,"setosa","C"),
+        (5.0,3.3,1.4,0.2,"setosa","C"),
+        (7.0,3.2,4.7,1.4,"versicolor","B"),
+        (6.4,3.2,4.5,1.5,"versicolor","B"),
+        (6.9,3.1,4.9,1.5,"versicolor","B"),
+        (5.5,2.3,4.0,1.3,"versicolor","B"),
+        (6.5,2.8,4.6,1.5,"versicolor","B"),
+        (5.7,2.8,4.5,1.3,"versicolor","E"),
+        (6.3,3.3,4.7,1.6,"versicolor","E"),
+        (4.9,2.4,3.3,1.0,"versicolor","E"),
+        (6.6,2.9,4.6,1.3,"versicolor","E"),
+        (5.2,2.7,3.9,1.4,"versicolor","F"),
+        (5.0,2.0,3.5,1.0,"versicolor","F"),
+        (5.9,3.0,4.2,1.5,"versicolor","F"),
+        (6.0,2.2,4.0,1.0,"versicolor","F"),
+        (6.1,2.9,4.7,1.4,"versicolor","F"),
+        (5.6,2.9,3.6,1.3,"versicolor","F"),
+        (6.7,3.1,4.4,1.4,"versicolor","A"),
+        (5.6,3.0,4.5,1.5,"versicolor","A"),
+        (5.8,2.7,4.1,1.0,"versicolor","A"),
+        (6.2,2.2,4.5,1.5,"versicolor","A"),
+        (5.6,2.5,3.9,1.1,"versicolor","E"),
+        (5.9,3.2,4.8,1.8,"versicolor","E"),
+        (6.1,2.8,4.0,1.3,"versicolor","E"),
+        (6.3,2.5,4.9,1.5,"versicolor","E"),
+        (6.1,2.8,4.7,1.2,"versicolor","E"),
+        (6.4,2.9,4.3,1.3,"versicolor","D"),
+        (6.6,3.0,4.4,1.4,"versicolor","D"),
+        (6.8,2.8,4.8,1.4,"versicolor","A"),
+        (6.7,3.0,5.0,1.7,"versicolor","A"),
+        (6.0,2.9,4.5,1.5,"versicolor","A"),
+        (5.7,2.6,3.5,1.0,"versicolor","A"),
+        (5.5,2.4,3.8,1.1,"versicolor","D"),
+        (5.5,2.4,3.7,1.0,"versicolor","D"),
+        (5.8,2.7,3.9,1.2,"versicolor","D"),
+        (6.0,2.7,5.1,1.6,"versicolor","D"),
+        (5.4,3.0,4.5,1.5,"versicolor","E"),
+        (6.0,3.4,4.5,1.6,"versicolor","E"),
+        (6.7,3.1,4.7,1.5,"versicolor","E"),
+        (6.3,2.3,4.4,1.3,"versicolor","E"),
+        (5.6,3.0,4.1,1.3,"versicolor","F"),
+        (5.5,2.5,4.0,1.3,"versicolor","A"),
+        (5.5,2.6,4.4,1.2,"versicolor","A"),
+        (6.1,3.0,4.6,1.4,"versicolor","A"),
+        (5.8,2.6,4.0,1.2,"versicolor","C"),
+        (5.0,2.3,3.3,1.0,"versicolor","C"),
+        (5.6,2.7,4.2,1.3,"versicolor","C"),
+        (5.7,3.0,4.2,1.2,"versicolor","C"),
+        (5.7,2.9,4.2,1.3,"versicolor","F"),
+        (6.2,2.9,4.3,1.3,"versicolor","E"),
+        (5.1,2.5,3.0,1.1,"versicolor","D"),
+        (5.7,2.8,4.1,1.3,"versicolor","C"),
+        (6.3,3.3,6.0,2.5,"virginica","C"),
+        (5.8,2.7,5.1,1.9,"virginica","C"),
+        (7.1,3.0,5.9,2.1,"virginica","D"),
+        (6.3,2.9,5.6,1.8,"virginica","C"),
+        (6.5,3.0,5.8,2.2,"virginica","C"),
+        (7.6,3.0,6.6,2.1,"virginica","F"),
+        (4.9,2.5,4.5,1.7,"virginica","F"),
+        (7.3,2.9,6.3,1.8,"virginica","C"),
+        (6.7,2.5,5.8,1.8,"virginica","B"),
+        (7.2,3.6,6.1,2.5,"virginica","B"),
+        (6.5,3.2,5.1,2.0,"virginica","B"),
+        (6.4,2.7,5.3,1.9,"virginica","E"),
+        (6.8,3.0,5.5,2.1,"virginica","E"),
+        (5.7,2.5,5.0,2.0,"virginica","B"),
+        (5.8,2.8,5.1,2.4,"virginica","B"),
+        (6.4,3.2,5.3,2.3,"virginica","B"),
+        (6.5,3.0,5.5,1.8,"virginica","B"),
+        (7.7,3.8,6.7,2.2,"virginica","F"),
+        (7.7,2.6,6.9,2.3,"virginica","F"),
+        (6.0,2.2,5.0,1.5,"virginica","F"),
+        (6.9,3.2,5.7,2.3,"virginica","F"),
+        (5.6,2.8,4.9,2.0,"virginica","F"),
+        (7.7,2.8,6.7,2.0,"virginica","F"),
+        (6.3,2.7,4.9,1.8,"virginica","D"),
+        (6.7,3.3,5.7,2.1,"virginica","D"),
+        (7.2,3.2,6.0,1.8,"virginica","D"),
+        (6.2,2.8,4.8,1.8,"virginica","D"),
+        (6.1,3.0,4.9,1.8,"virginica","D"),
+        (6.4,2.8,5.6,2.1,"virginica","D"),
+        (7.2,3.0,5.8,1.6,"virginica","D"),
+        (7.4,2.8,6.1,1.9,"virginica","B"),
+        (7.9,3.8,6.4,2.0,"virginica","B"),
+        (6.4,2.8,5.6,2.2,"virginica","F"),
+        (6.3,2.8,5.1,1.5,"virginica","F"),
+        (6.1,2.6,5.6,1.4,"virginica","F"),
+        (7.7,3.0,6.1,2.3,"virginica","B"),
+        (6.3,3.4,5.6,2.4,"virginica","E"),
+        (6.4,3.1,5.5,1.8,"virginica","E"),
+        (6.0,3.0,4.8,1.8,"virginica","E"),
+        (6.9,3.1,5.4,2.1,"virginica","E"),
+        (6.7,3.1,5.6,2.4,"virginica","B"),
+        (6.9,3.1,5.1,2.3,"virginica","B"),
+        (5.8,2.7,5.1,1.9,"virginica","A"),
+        (6.8,3.2,5.9,2.3,"virginica","A"),
+        (6.7,3.3,5.7,2.5,"virginica","A"),
+        (6.7,3.0,5.2,2.3,"virginica","E"),
+        (6.3,2.5,5.0,1.9,"virginica","E"),
+        (6.5,3.0,5.2,2.0,"virginica","E"),
+        (6.2,3.4,5.4,2.3,"virginica","A"),
+        (5.9,3.0,5.1,1.8,"virginica","A") ]
 
-group :: DataString
-group = (["group"],["World","World","USA","USA","World","World","World","USA","World","World","World","USA","USA","USA","USA","USA"],["USA", "World"])
+sepalLength = DataDouble "sepal length" [a | (a,_,_,_,_,_)<-iris] (4.0,8.0)
+sepalWidth  = DataDouble "sepal width"  [a | (_,a,_,_,_,_)<-iris] (2.0,5.0)
+petalLength = DataDouble "petal length" [a | (_,_,a,_,_,_)<-iris] (1.0,7.0)
+petalWidth  = DataDouble "petal width"  [a | (_,_,_,a,_,_)<-iris] (0.0,3.0)
+species     = DataString "species"      [a | (_,_,_,_,a,_)<-iris] ["setosa","versicolor","virginica"]
+typeall     = DataString "type all"     [a | (_,_,_,_,_,a)<-iris] ["A","B","C","D","E","F"]
 
-team :: DataString
-team = (["Team"],["C","B","A","C","B","A","B","B","B","A","C","C","C","B","A","A"],["A", "B", "C"])
+doubleparts = 10.0
+makepoint (x,y) = p2 (x,y)
 
-pop2000 :: DataInt
-pop2000 = (["Population"],[26.2,18.4,16.7,14.3,9.6,7.2,7.1,6.4,5.3,4.8,2.9,2.1,1.7,1.3,0.8,0.5],[0,4..32])
+createDoublelist :: (Double,Double) -> Double -> [String]
+createDoublelist (a,b) n = [show (a+(i*(b-a)/n)) | i <- [0..n] ]
 
-cross :: DataString -> DataString -> [DataString]
-cross ([a],b,c) ([x],y,z) = [([a,x,(z!!t)],(resulting b y (z!!t)),c) |t <- [0..((length z) - 1)]]
+coordCartesian :: DataFrame -> DataFrame -> DiaR2
+coordCartesian (DataDouble a1 b1 c1) (DataDouble a2 b2 c2) = position (zip (map makepoint [(-0.85,0),(0,-0.85),(0,0)]) [text a1 # fontSizeL 0.1 # rotateBy (1/4), text a2 # fontSizeL 0.1, (verticksD (createDoublelist c1 doubleparts) <> horizonsD (createDoublelist c2 doubleparts))])
+coordCartesian (DataDouble a1 b1 c1) (DataString a2 b2 c2) = position (zip (map makepoint [(-0.85,0),(0,-0.85),(0,0)]) [text a1 # fontSizeL 0.1 # rotateBy (1/4), text a2 # fontSizeL 0.1, (verticksD (createDoublelist c1 doubleparts) <> horizonsS c2)])
+coordCartesian (DataString a1 b1 c1) (DataDouble a2 b2 c2) = position (zip (map makepoint [(-0.85,0),(0,-0.85),(0,0)]) [text a1 # fontSizeL 0.1 # rotateBy (1/4), text a2 # fontSizeL 0.1, (verticksS c1 <> horizonsD (createDoublelist c2 doubleparts))])
+coordCartesian (DataString a1 b1 c1) (DataString a2 b2 c2) = position (zip (map makepoint [(-0.85,0),(0,-0.85),(0,0)]) [text a1 # fontSizeL 0.1 # rotateBy (1/4), text a2 # fontSizeL 0.1, (verticksS c1 <> horizonsS c2)])
 
-resulting :: [String] -> [String] -> String -> [String]
-resulting xs ys c = [if ((ys!!t) == c) then (xs!!t) else "None"|t <-[0..((length xs) - 1)]]
+verticksD :: [String] -> DiaR2
+verticksD ys = position (zip ([makepoint (-0.52,-0.5)]) [(mconcat [ text t # fontSizeL 0.025 # moveTo ((-0.14)^&(y*1.0)) | (y,t) <- ylist]) <> (mconcat [fromVertices [0.0^&(y*1.0), 0.02^&(y*1.0)] | (y,t) <- ylist])] )
+        where ylist = zip [(fromIntegral d)/(fromIntegral ((length ys)-1)) | d <- [0..((length ys) - 1)]] ys
 
-nest :: DataString -> DataString -> [DataString]
-nest ([a],b,c) ([x],y,z) = [([a,x,(z!!t)],(resulting b y (z!!t)),(removeN (unique (resulting b y (z!!t))))) |t <- [0..((length z) - 1)]]
+verticksS :: [String] -> DiaR2
+verticksS ys = position (zip ([makepoint (-0.52,-0.5)]) [(mconcat [ text t # fontSizeL 0.025 # moveTo ((-0.14)^&(y*1.0)) | (y,t) <- ylist]) <> (mconcat [fromVertices [0.0^&(y*1.0), 0.02^&(y*1.0)] | (y,t) <- ylist])] ) 
+        where ylist = zip [(fromIntegral d)/(fromIntegral ((length ys)+1)) | d <- [1..(length ys)]] ys
 
-has :: (Eq a) => [a] -> a -> Bool
-has [] _ = False
-has (x:xs) a
-  | x == a    = True
-  | otherwise = has xs a
+horizonsD :: [String] -> DiaR2
+horizonsD xs = position (zip ([makepoint (-0.5,-0.52)]) [(mconcat [ text t # fontSizeL 0.025  # rotateBy (1/4) # moveTo ((x*1.0)^&(-0.14)) | (x,t) <- xlist]) <> (mconcat [fromVertices [(x*1.0)^&0.0, (x*1.0)^&0.02] | (x,t) <- xlist])] )
+        where xlist = zip [(fromIntegral d)/(fromIntegral ((length xs)-1)) | d <- [0..((length xs) - 1)]] xs
 
-unique :: (Eq a) => [a] -> [a]
-unique [] = []
-unique (x:xs)
-  | has xs x  = unique xs
-  | otherwise = x : unique xs
+horizonsS :: [String] -> DiaR2
+horizonsS xs = position (zip ([makepoint (-0.5,-0.52)]) [(mconcat [ text t # fontSizeL 0.025  # rotateBy (1/4) # moveTo ((x*1.0)^&(-0.14)) | (x,t) <- xlist]) <> (mconcat [fromVertices [(x*1.0)^&0.0, (x*1.0)^&0.02] | (x,t) <- xlist])] )
+        where xlist = zip [(fromIntegral d)/(fromIntegral ((length xs)+1)) | d <- [1..(length xs)]] xs
 
-removeN :: [String] -> [String]
-removeN xs = [x |x <- xs, x /= "None"]
+scatterplot :: [(Double, Double, Double, Colour Double)] -> DiaR2
+scatterplot a = position (zip (map makepoint [(y,x)|(x,y,_,_)<-a]) [(circle size #fc fillcolor) |(_,_,size,fillcolor) <-a])
 
-createpoints :: [(Double,String)] -> [String]-> [Int] -> [(Double,Double)]
-createpoints a b c = [(z,(fromIntegral t+1)/(fromIntegral (length b) + 1))|(z,r)<-rN, t <- [0..((length b) - 1)], r == (b!!t) ]
-                     where rN = [(x/(fromIntegral (c!!0)),y) |(x,y)<- a, y /= "None"]
+createD :: DataFrame -> [Double]
+createD (DataDouble _ xs (a,b)) = [(x-a)/(b-a)| x<-xs]
+createD (DataString _ xs ds) = [(relate x (zip ds [1.0..]))/fromIntegral  ((length ds)+ 1)| x<-xs]
+	
+relate :: Eq a => a -> [(a, b)] -> b
+relate c = snd.head.dropWhile ((/=c).fst)
 
-plotfunction :: [DataString] -> DataInt -> Dia
-plotfunction xs ys = position (zip (map makepoint [(0,0),((fromIntegral(length xs)-1),0)]) [( hcat' (with & sep .~ 0.25 ) [plotSingle x ys | x<-xs] ), (rect (2*fromIntegral(length xs)) 2 #fc grey)])
+joinE :: [Double] -> [Double] -> [Double] -> [Colour Double] -> [(Double, Double, Double, Colour Double)]
+joinE x y z w = [(x!!i, y!!i, z!!i, w!!i)| i <- [0..((length x) - 1)]]
 
-plotSingle :: DataString -> DataInt -> Dia
-plotSingle (a,b,c) ([x],y,z) = if (length a) == 1 then plotmake [x,a!!0] c (map show z) (createpoints [(y!!t,b!!t) | t <- [0..((length y) - 1)]] c [last(z)]) else plotmake [x,a!!0,a!!2] c (map show z) (createpoints [(y!!t,b!!t) | t <- [0..((length y) - 1)]] c [last(z)])
+example :: DiaR2
+example = foogc1 <> foocb <> (square 1 #fc white) <> (square 2 #fc grey)
 
-makepoint (x,y) = p2 (x,y) 
+fooga :: DiaR2
+fooga = scatterplot (joinE (createD sepalWidth) (createD sepalLength) (repeat 0.01) (repeat black))# translate (r2 (-0.5, -0.5))
 
-plotmake :: [String] -> [String] -> [String] -> [(Double,Double)] -> Dia
-plotmake a b c d = if (length a) == 2 then (plotsimple a b c d) else position (zip (map makepoint [(0,0.75),(0,0)]) [label (a!!2),plotsimple a b c d])
+foogb :: DiaR2
+foogb = scatterplot (joinE (createD sepalWidth) (createD sepalLength) (repeat 0.01) (foo3))# translate (r2 (-0.5, -0.5))
 
-label :: String -> Dia
-label a = text a # fontSizeL 0.1 <> rect 1 0.2 #fc lightblue
+foogc :: DiaR2
+foogc = scatterplot (joinE (createD sepalWidth) (createD sepalLength) foo4 foo3)# translate (r2 (-0.5, -0.5))
 
-plotsimple :: [String] -> [String] -> [String] -> [(Double,Double)] -> Dia
-plotsimple a b c d = position (zip (map makepoint [(-0.5,-0.5),(0,0)]) [plotpoints d, plotbase a b c] )
+fooca :: DiaR2
+fooca = coordCartesian sepalWidth sepalLength
 
-plotpoints :: [(Double,Double)] -> Dia
-plotpoints a = position (zip (map makepoint [(v,c)|(c,v)<-a]) (repeat dot))
+foocb :: DiaR2
+foocb = coordCartesian petalWidth petalLength
 
-dot :: Dia
-dot = circle 0.01 # fc red
+foogc1 :: DiaR2
+foogc1 = scatterplot (joinE (createD petalWidth) (createD petalLength) foo5 foo3)# translate (r2 (-0.5, -0.5))
 
-plotbase :: [String] -> [String] -> [String]-> Dia
-plotbase a b c =  position (zip (map makepoint [(-0.75,0),(0,-0.85),(0,0)]) [text (a!!0)# fontSizeL 0.1 # rotateBy (1/4),text (a!!1)# fontSizeL 0.1,(verticals c <> horizons b)])
+foo3 = mapdisContinous species [white, blue, red]
+foo4 = mapdisContinous typeall [0.01, 0.011, 0.012, 0.013, 0.014, 0.015]
+foo5 = mapContinous sepalWidth (0.01,0.015)
 
-verticals :: [String] -> Dia
-verticals xs = position (zip ([makepoint (-0.52,-0.5)]) [(mconcat [ text t # fontSizeL 0.035 # moveTo ((-0.04)^&(y*1)) | (y,t) <- ylist]) <> (mconcat [fromVertices [0^&(y*1), 0.02^&(y*1)] | (y,t) <- ylist])] ) `atop` square 1 #fc white 
-        where ylist = zip [(fromIntegral d)/(fromIntegral ((length xs)-1)) | d <- [0..((length xs) - 1)]] xs
+mapdisContinous :: DataFrame -> [a] -> [a]
+mapdisContinous (DataString _ xs ds) map = [ relate x (zip ds map)| x<-xs]
+mapdisContinous (DataDouble _ xs ds) map = error "something"
 
-horizons :: [String] -> Dia
-horizons ys = position (zip ([makepoint (-0.5,-0.52)]) [(mconcat [ text t # fontSizeL 0.035  # rotateBy (1/4) # moveTo ((x*1)^&(-0.14)) | (x,t) <- xlist]) <> (mconcat [fromVertices [(x*1)^&0, (x*1)^&0.02] | (x,t) <- xlist])] )
-        where xlist = zip [(fromIntegral d)/(fromIntegral ((length ys)+1)) | d <- [1..(length ys)]] ys
-
-example :: Dia
-example = plotfunction c1  pop2000
-
-a1 = [city]
-b1 = (cross city team)
-b2 = (cross city group)
-c1 = (nest city team)
-c2 = (nest city group)
+mapContinous :: DataFrame -> (Double, Double) -> [Double]
+mapContinous (DataString _ xs ds) (a,b) = error "something"
+mapContinous (DataDouble _ xs (d1, d2)) (a,b) = [a + ((x*(b-a))/(d2-d1)) | x<-xs]
 
 main = mainWith example
-
-
-
-                                                                       
-                                       
-
-
- 
